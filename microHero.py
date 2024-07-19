dd7055790b32c3604849047b6d1bd247d8044fd5
Maxim Eremeev <shlimazl@gmail.com>
2024-07-19 09:27:17 +0300

    Updated resource unpacker and welcome screen

--- microHero.py
+++ microHero.py
@@ -8,13 +8,10 @@ FullNames = []
 
 Vocations = ["Barbarian", "Wizard", "Warrior", "Cleric", "Necromancer"]
 
-TempPrefixes = []
 Prefixes = []
-TempItems = []
-Items = []
-TempPostfixes = []
+Cores = []
 Postfixes = []
-ItemList = []
+Enemies = []
 
 Player = None
 
@@ -26,7 +23,7 @@ class User:
         return self.Name
 
 class Character:
-    def __init__(self, Name, HP, MaxHP, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot):
+    def __init__(self, Name, HP, MaxHP, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot, InventorySize=0, InventoryMaxSize=10):
         self.Name = Name
         self.HP = HP
         self.MaxHP = MaxHP
@@ -37,43 +34,47 @@ class Character:
         self.MinDmg = MinDmg
         self.MaxDmg = MaxDmg
         self.Prot = Prot
-
+        self.Inventory = None
+        self.InventorySize = InventorySize
+        self.InventoryMaxSize = InventoryMaxSize
 
     def __repr__(self):
         return f"""
-                {self.Name}, the {self.Vocation}
+                {self.Name} / The {self.Vocation}
+                HP: {self.HP} / {self.MaxHP}
                 STR: {self.Str}
                 INT: {self.Int}
                 FAI: {self.Fai}
 
                 DMG: {self.MinDmg}—{self.MaxDmg}
-                Prot: {self.Prot
-        }"""
+                Prot: {self.Prot}
+                
+                Inventory: {self.Inventory}"""
     
 class Enemy:
-    def __init__(self, Name, HP, MaxHP, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot):
+    def __init__(self, Name, MinHP, MaxHP, Vocation, MinStr, MaxStr, MinInt, MaxInt, MinFai, MaxFai, MinDmgMin, MinDmgMax, MaxDmgMin, MaxDmgMax,
+                 ProtMin, ProtMax, Tier, Exp):
         self.Name = Name
-        self.HP = HP
+        self.MinHp = MinHP
         self.MaxHP = MaxHP
         self.Vocation = Vocation
-        self.Str = Str
-        self.Int = Int
-        self.Fai = Fai
-        self.MinDmg = MinDmg
-        self.MaxDmg = MaxDmg
-        self.Prot = Prot
-
+        self.MinStr = MinStr
+        self.MaxStr = MaxStr
+        self.MinInt = MinInt
+        self.MaxInt = MaxInt
+        self.MinFai = MinFai
+        self.MaxFai = MaxFai
+        self.MinDmgMin = MinDmgMin
+        self.MinDmgMax = MinDmgMax
+        self.MaxDmgMin = MaxDmgMin
+        self.MaxDmgMax = MaxDmgMax
+        self.ProtMin = ProtMin
+        self.ProtMax = ProtMax
+        self.Tier = Tier
+        self.Exp = Exp
 
     def __repr__(self):
-        return f"""
-                {self.Name}, the {self.Vocation}
-                STR: {self.Str}
-                INT: {self.Int}
-                FAI: {self.Fai}
-
-                DMG: {self.MinDmg}—{self.MaxDmg}
-                Prot: {self.Prot
-        }"""
+        return f"""{self.Name} / The {self.Vocation}"""
     
 class Item:
     def __init__(self, Name):
@@ -81,7 +82,7 @@ class Item:
 
     def __repr__(self):
         return f"{self.Name}"
-
+    
 class Prefix:
     def __init__(self, Name, Effect, Modifier, Tier, PosNeg, Description, Price):
         self.Name = Name
@@ -123,6 +124,10 @@ class Postfix:
 def unpack_lists():
     global Names
     global Nicknames
+    global Prefixes
+    global Cores
+    global Postfixes
+    global Enemies
     
     with open("names.lst") as NamesLst:
         Text = NamesLst.read()
@@ -131,6 +136,40 @@ def unpack_lists():
     with open("nicknames.lst") as NicknamesLst:
         Text = NicknamesLst.read()
         Nicknames = Text.split(",")
+
+    with open("item_prefixes.lst") as PrefixesLst:
+        Text = PrefixesLst.read()
+        TempLst = Text.split(";")
+        for Element in TempLst:
+            Stat = Element.split(",")
+            TempPrefix = Prefix(Stat[0], Stat[1], Stat[2], Stat[3], Stat[4], Stat[5], Stat[6])
+            Prefixes.append(TempPrefix)
+        print(Prefixes)
+
+    with open("item_cores.lst") as CoresLst:
+        Text = CoresLst.read()
+        TempLst = Text.split(";")
+        for Element in TempLst:
+            Stat = Element.split(",")
+            TempCore = Core(Stat[0], Stat[1], Stat[2], Stat[3], Stat[4], Stat[5], Stat[6])
+            Cores.append(TempCore)
+        print(Cores)
+
+    with open("item_postfixes.lst") as PostfixesLst:
+        Text = PostfixesLst.read()
+        TempLst = Text.split(";")
+        for Element in TempLst:
+            Stat = Element.split(",")
+            TempPostfix = Postfix(Stat[0], Stat[1], Stat[2], Stat[3], Stat[4], Stat[5])
+            Postfixes.append(TempPostfix)
+        print(Postfixes)
+
+    with open("enemies.lst") as EnemiesLst:
+        Text = EnemiesLst.read()
+        TempLst = Text.split("\n")
+        for Element in TempLst:
+            Enemies.append(Element)
+        print(Enemies)
 
 def create_player(Name):
     global Player
@@ -198,20 +237,47 @@ def create_character():
             CharProt = random.randint(5,8)
     
     Char = Character(CharName, CharHP, CharMaxHP, CharVoc, CharStr, CharInt, CharFai, CharMinDMG, CharMaxDMG, CharProt)
+    Char.Inventory = []
     Player.Char = Char
+
+def welcome():
+    print(f"""
+            Hello, {Player.Name}! Welcome to MicroHero!
+            Your champion for today is:
+            {Player.Char}""")
+#            the {Player.Char.Vocation}
+#            HP: {Player.Char.HP} / {Player.Char.MaxHP}
+#            STR: {Player.Char.Str}
+#            INT: {Player.Char.Int}
+#            FAI: {Player.Char.Fai}
+#            Min.DMG: {Player.Char.MinDmg}
+#            Max.DMG: {Player.Char.MaxDmg}
+#            Protection: {Player.Char.Prot}
+
+    print("""
+          There is no real goal in MicroHero — just the path
+          on which your champion will become stronger and stronger.
+          Help them overcome untold dangers, share in their loot,
+          equip them and guide them to riches and glory!""")
+    
+    start = input("\n             Onwards! Enter any symbol to continue... ")
+
+    choose_encounter()
+
+def choose_encounter():
+    if (random.randint(1,100) + Player.Char.Fai) <= 30:
+        positive_encounter()
+    else:
+        battle()
+
+def positive_encounter():
+    pass
+
+def battle():
+    pass
 
 PlayerName = input("\nWhat is your name? ")
 unpack_lists()
 create_player(PlayerName)
 create_character()
-print(f"""
-        Hello, {Player.Name}! Welcome to MicroHero!
-        Your champion for today is {Player.Char.Name}, the {Player.Char.Vocation}
-        HP: {Player.Char.HP} / {Player.Char.MaxHP}
-        STR: {Player.Char.Str}
-        INT: {Player.Char.Int}
-        FAI: {Player.Char.Fai}
-        Min.DMG: {Player.Char.MinDmg}
-        Max.DMG: {Player.Char.MaxDmg}
-        Protection: {Player.Char.Prot}
-        """)
+welcome()
