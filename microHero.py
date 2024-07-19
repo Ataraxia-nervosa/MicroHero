50d92dd7e3024626590332d2be00b66d59275a60
Maxim Eremeev <shlimazl@gmail.com>
2024-07-19 07:57:36 +0300

    Added some randomizer features and the initial greeting screen

--- microHero.py
+++ microHero.py
@@ -2,7 +2,7 @@ import random
 
 day = 0
 PlayerCount = 0
-CharacterNames = []
+Names = []
 Nicknames = []
 FullNames = []
 
@@ -16,9 +16,9 @@ TempPostfixes = []
 Postfixes = []
 ItemList = []
 
-store_visited = False
+Player = None
 
-class Player:
+class User:
     def __init__(self, Name):
         self.Name = Name
     
@@ -26,8 +26,10 @@ class Player:
         return self.Name
 
 class Character:
-    def __init__(self, Name, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot):
+    def __init__(self, Name, HP, MaxHP, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot):
         self.Name = Name
+        self.HP = HP
+        self.MaxHP = MaxHP
         self.Vocation = Vocation
         self.Str = Str
         self.Int = Int
@@ -49,8 +51,10 @@ class Character:
         }"""
     
 class Enemy:
-    def __init__(self, Name, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot):
+    def __init__(self, Name, HP, MaxHP, Vocation, Str, Int, Fai, MinDmg, MaxDmg, Prot):
         self.Name = Name
+        self.HP = HP
+        self.MaxHP = MaxHP
         self.Vocation = Vocation
         self.Str = Str
         self.Int = Int
@@ -117,31 +121,97 @@ class Postfix:
         return f"{self.Name}"
     
 def unpack_lists():
-    with open("micro_arena_names.lst") as NamesLst:
-        CharacterNames = NamesLst.read().split(",")
+    global Names
+    global Nicknames
+    
+    with open("names.lst") as NamesLst:
+        Text = NamesLst.read()
+        Names = Text.split(",")
 
-    with open("micro_arena_nicknames.lst") as NicknamesLst:
-        Nicknames = NicknamesLst.read().split(",") 
+    with open("nicknames.lst") as NicknamesLst:
+        Text = NicknamesLst.read()
+        Nicknames = Text.split(",")
+
+def create_player(Name):
+    global Player
+    Player = User(Name)
 
 def generate_name(List1, List2):
-    A = random.randint(0,len(CharacterNames)-1)
+    A = random.randint(0,len(Names)-1)
     B = random.randint(0,len(Nicknames)-1)
     FullName = str(List1[A] + List2[B])
-
-def generate_hp(VNum):
-    match VNum:
-        case 1:
-            return random.randint(50, 150)
-        case 2:
-            return random.randint(40, 60)
-        case 3:
-            return random.randint(45, 100)
-        case 4:
-            return random.randint(45,80)
-        case 5:
-            return random.randint(40, 60)
+    return FullName
 
 def create_character():
-    CharVoc = random.randint(1,5)
-    CharHP = generate_hp(CharVoc)
+    CharName = generate_name(Names, Nicknames)
+    VocNum = random.randint(1,5)
+    match VocNum:
+        case 1:
+            CharVoc = "Barbarian"
+            CharHP = random.randint(50,150)
+            CharMaxHP = CharHP
+            CharStr = random.randint(12,20)
+            CharInt = random.randint(5,10)
+            CharFai = random.randint(7,15)
+            CharMinDMG = random.randint(15,20)
+            CharMaxDMG = random.randint(30,40)
+            CharProt = random.randint(8,10)
+        case 2:
+            CharVoc = "Wizard"
+            CharHP = random.randint(30,60)
+            CharMaxHP = CharHP
+            CharStr = random.randint(5,8)
+            CharInt = random.randint(12,20)
+            CharFai = random.randint(5,8)
+            CharMinDMG = random.randint(20,30)
+            CharMaxDMG = random.randint(40,60)
+            CharProt = random.randint(2,6)
+        case 3:
+            CharVoc = "Warrior"
+            CharHP = random.randint(60,80)
+            CharMaxHP = CharHP
+            CharStr = random.randint(10,18)
+            CharInt = random.randint(7,15)
+            CharFai = random.randint(5,8)
+            CharMinDMG = random.randint(12,18)
+            CharMaxDMG = random.randint(25,30)
+            CharProt = random.randint(15,20)
+        case 4:
+            CharVoc = "Cleric"
+            CharHP = random.randint(35,50)
+            CharMaxHP = CharHP
+            CharStr = random.randint(7,12)
+            CharInt = random.randint(8,13)
+            CharFai = random.randint(12,20)
+            CharMinDMG = random.randint(8,15)
+            CharMaxDMG = random.randint(18,25)
+            CharProt = random.randint(5,8)
+        case 5:
+            CharVoc = "Necromancer"
+            CharHP = random.randint(25,55)
+            CharMaxHP = CharHP
+            CharStr = random.randint(5,8)
+            CharInt = random.randint(9,17)
+            CharFai = random.randint(2,4)
+            CharMinDMG = random.randint(9,17)
+            CharMaxDMG = random.randint(18,20)
+            CharProt = random.randint(5,8)
+    
+    Char = Character(CharName, CharHP, CharMaxHP, CharVoc, CharStr, CharInt, CharFai, CharMinDMG, CharMaxDMG, CharProt)
+    Player.Char = Char
 
+PlayerName = input("\nWhat is your name? ")
+unpack_lists()
+create_player(PlayerName)
+create_character()
+print(f"""
+        Hello, {Player.Name}! Welcome to MicroHero!
+        Your champion for today is {Player.Char.Name}, the {Player.Char.Vocation}
+        HP: {Player.Char.HP} / {Player.Char.MaxHP}
+        STR: {Player.Char.Str}
+        INT: {Player.Char.Int}
+        FAI: {Player.Char.Fai}
+        Min.DMG: {Player.Char.MinDmg}
+        Max.DMG: {Player.Char.MaxDmg}
+        Protection: {Player.Char.Prot}
+        """)
